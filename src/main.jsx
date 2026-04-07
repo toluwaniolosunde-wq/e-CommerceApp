8b37b7ff0e8a6bfd0488cbab50360f0df101d6c3
toluwaniolosunde-wq <toluwaniolosunde@gmail.com>
2026-04-07 14:41:20 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import { store } from './store/store.jsx'
 import { Provider} from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+  <StrictMode>
   <Provider store={store}>
           <App />
   </Provider>
-  // </StrictMode>,
+  </StrictMode>,
 )
